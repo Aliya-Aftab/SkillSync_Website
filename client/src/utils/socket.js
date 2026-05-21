fc21fb856a393cc64dacae3b03d3d37c190c0609
Aliya Aftab <aftabaliya40@gmail.com>
2026-05-21 22:00:36 +0530

    fixed socket.io

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -3,8 +3,15 @@ import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io(BASE_URL);
+    // Added withCredentials for local testing
+    return io(BASE_URL, {
+      withCredentials: true,
+    });
   } else {
-    return io("/", { path: "/api/socket.io" });
+    // Added withCredentials for Production (Vercel -> Render)
+    return io(BASE_URL, { 
+      path: "/api/socket.io",
+      withCredentials: true, 
+    });
   }
 };
